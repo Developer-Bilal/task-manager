0e545bf297a0b74f52f153c01fcb386b184f4749
Muhammad Bilal <bilalchanna67@gmail.com>
2025-03-20 05:22:12 +0500

    frontend added register and login page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,12 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  let session = false;
+
+  if (!session) {
+    redirect("/register");
+  }
+
   return (
     <div>
       <div>Task Manager App</div>
